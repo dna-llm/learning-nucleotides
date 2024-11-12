c1f19cff9b66405f6fc96a499be00bcc935e0b08
GOESTERN-0907385 <masprang@uni-mainz.de>
2024-11-12 12:14:00 +0100

    nhr template

--- experiments/slurm/sbatch_array_NHR_template.sh
+++ experiments/slurm/sbatch_array_NHR_template.sh
@@ -10,7 +10,7 @@
 #SBATCH -e slurm_out/2d_array.%A_%a.err
 
 #SBATCH --time=01:00:00
-#SBATCH --array=0-6
+#SBATCH --array=0-3
 
 #SBATCH -G A100:1
 
@@ -22,14 +22,14 @@ python -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token('hf_
 
 # Array of config files
 CONFIGS=(
-    experiments/configs/wavelet/wavelet-14m-2048-two_d.yaml
     experiments/configs/denseformer/denseformer_2048/denseformer-160m-2048-two_d.yaml
     experiments/configs/denseformer/denseformer_2048/denseformer-410m-2048-two_d.yaml
     experiments/configs/pythia/pythia_2048/pythia-410m-2028-two_d.yaml
     experiments/configs/pythia/pythia_2048/pythia-160m-2028-two_d.yaml
-    experiments/configs/evo/evo_2048/evo-410m-2048-two_d.yaml
-    experiments/configs/evo/evo_2048/evo-160m-2048-two_d.yaml
 )
+    # experiments/configs/wavelet/wavelet-14m-2048-two_d.yaml
+    # experiments/configs/evo/evo_2048/evo-410m-2048-two_d.yaml
+    # experiments/configs/evo/evo_2048/evo-160m-2048-two_d.yaml
 
 # Get the current array task ID
 CONFIG=${CONFIGS[$SLURM_ARRAY_TASK_ID]}
