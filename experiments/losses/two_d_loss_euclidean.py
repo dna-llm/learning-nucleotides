6045f8acd44d3b44bdf134eba870ea402ff38bb7
Hassan <69965978+hssn-20@users.noreply.github.com>
2024-11-21 19:51:05 +0000

    Update two_d_loss_euclidean.py

--- experiments/losses/two_d_loss_euclidean.py
+++ experiments/losses/two_d_loss_euclidean.py
@@ -32,11 +32,19 @@ class TwoDRepLossEuclidean(Trainer):
         input_ids = inputs.pop("input_ids")
         logits = self.get_logits(model(input_ids[:, :-1]))
         labels = input_ids[:, 1:]
-
+        label_one_hot = F.one_hot(labels.long(), num_classes=8).float()
+        
+        mask = (labels > 2).float() * (labels < 7).float()
+        mask_batch_size = mask.shape[0] 
+        mask_o = mask
+        mask =  mask.reshape(mask_batch_size * 2047,1)
+        #print(mask)
+        # CE Loss 
+        loss_fct = torch.nn.CrossEntropyLoss()
+      #  loss = loss_fct((logits*mask), (label_one_hot*mask)).mean()
+        loss = loss_fct(logits.reshape(mask_batch_size*2047,8) * mask, label_one_hot.reshape(mask_batch_size*2047,8) * mask).mean()
         # Use softmax probabilities
         probabilities = F.softmax(logits, dim=-1)
-        mask = (labels > 2).float() * (labels < 7).float()
-
         transform_matrix = torch.tensor([
             [0.0, 0.0],
             [0.0, 0.0],
@@ -48,16 +56,46 @@ class TwoDRepLossEuclidean(Trainer):
             [0.0, 0.0]
         ]).to(device)
         
+        energy_matrix = torch.tensor([
+            [0.0, 0.0],
+            [0.0, 0.0],
+            [0.0, 0.0],
+            [1, 55.0],
+            [1, 45.0],
+            [1, 53.5],
+            [1, 45.0],
+            [0.0, 0.0]
+        ]).to(device)
+        
         pred_transformed = torch.matmul(probabilities, transform_matrix)
+        pred_cumsum = pred_transformed.cumsum(2)
+        
         label_one_hot = F.one_hot(labels.long(), num_classes=8).float()
         label_transformed = torch.matmul(label_one_hot, transform_matrix)
-        
-        pred_cumsum = torch.cumsum(pred_transformed, dim=1)
-        label_cumsum = torch.cumsum(label_transformed, dim=1)
-        
+        label_cumsum = label_transformed.cumsum(2)
+
+
+        #mask away irrelevant logits/labels
+        label_cumsum = label_cumsum * (mask_o.reshape(mask_batch_size ,2047,1))
+        pred_cumsum = pred_cumsum * (mask_o.reshape(mask_batch_size, 2047,1) )
         diff = pred_cumsum - label_cumsum
-        mask = torch.unsqueeze(mask, -1)
-        diff = diff * mask   
         
-        geometric_loss = diff.sum(1).mean(0).sum()
-        return geometric_loss
+        geometric_loss = torch.abs(diff).mean(1).mean(1).sum() 
+
+        pred_transformed = torch.matmul(probabilities, energy_matrix)
+        pred_cumsum = pred_transformed.cumsum(2)
+        
+        label_one_hot = F.one_hot(labels.long(), num_classes=8).float()
+        label_transformed = torch.matmul(label_one_hot, energy_matrix)
+        label_cumsum = label_transformed.cumsum(2)
+
+
+        #mask away irrelevant logits/labels
+        label_cumsum = label_cumsum * (mask_o.reshape(mask_batch_size ,2047,1))
+        pred_cumsum = pred_cumsum * (mask_o.reshape(mask_batch_size, 2047,1) )
+        diff = pred_cumsum - label_cumsum
+        
+        energy_loss = torch.abs(diff).mean(1).mean(1).sum() 
+        final_loss = loss + geometric_loss + energy_loss
+
+        return final_loss
