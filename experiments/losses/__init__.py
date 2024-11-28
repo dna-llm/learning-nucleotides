495dc7df8e956d236c3b23f54b2064993e53f1b6
Hassan <69965978+hssn-20@users.noreply.github.com>
2024-11-28 00:55:52 +0000

    Update __init__.py

--- experiments/losses/__init__.py
+++ experiments/losses/__init__.py
@@ -5,3 +5,4 @@ from .ph_loss import PHLoss
 from .reconstruction_loss import VAELoss
 from .two_d_loss_euclidean import TwoDRepLossEuclidean
 from .two_d_plus_ce_loss import TwoDRepL2CELoss
+from .discrete_ce_loss import  Discrete_CE_Loss
